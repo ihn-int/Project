5fbafd4d72d20aa4d745a3f694ad29d2948b9e5d
ihn-int <69340857+ihn-int@users.noreply.github.com>
2022-03-30 14:08:08 +0800

    Add files via upload

---
+++ Select Scene/List_Control.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class List_Control : MonoBehaviour
+{
+    public GameObject[] List = new GameObject[3];
+    Vector3 pad = new Vector3(50, 0, 0);
+
+    bool moving, direct;
+    int delayTime=20,time;
+    byte order,max;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        moving = false;
+        order = 0;
+        time = 0;
+        max = (byte)(List.Length - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (!moving)
+		{
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				if (order < max)
+				{
+                    moving = true;
+                    direct = true;
+                    order++;
+                    time = delayTime;
+				}
+			}
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				if (order > 0)
+				{
+                    moving = true;
+                    direct = false;
+                    order--;
+                    time = delayTime;
+				}
+			}
+			if (Input.GetKeyDown(KeyCode.Return))
+			{
+                database.type = order;
+                SceneManager.LoadScene("Game Scene");
+			}
+			if (Input.GetKeyDown(KeyCode.Escape))
+			{
+                SceneManager.LoadScene("Menu Scene");
+			}
+		}
+		else
+		{
+            if (time > 0)
+            {
+                if (direct)
+                {
+                    transform.position -= pad;
+				}
+				else
+				{
+                    transform.position += pad;
+				}
+                time--;
+			}
+			else
+			{
+                moving = false;
+			}
+		}
+    }
+}
---
+++ Start Scene/Manager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Manager : MonoBehaviour
+{
+    const int size = 3;
+    public TextMeshProUGUI[] texts = new TextMeshProUGUI[size];
+    public Image[] button = new Image[size];
+    public TextMeshProUGUI demo;
+    byte order;
+    public Image point;
+
+    Vector2 vec = new Vector2(0, 90);
+
+    Color invisible;
+    bool status;
+    bool page;
+    string play = "Press space to shoot bullet.\nPress left shift can low your speed.\n" +
+            "Press z can use bomb.\nEscape key is for pause.";
+    void demonstration(bool show)
+	{
+		if (show)
+		{
+            foreach(TextMeshProUGUI t in texts)
+			{
+                t.alpha = 0;
+			}
+            foreach(Image i in button)
+			{
+                invisible = i.color;
+                invisible.a = 0;
+                i.color = invisible;
+			}
+            invisible = point.color;
+            invisible.a = 0;
+            point.color = invisible;
+            demo.alpha = 255;
+		}
+		else
+		{
+            foreach(TextMeshProUGUI t in texts)
+			{
+                t.alpha = 255;
+			}
+            foreach(Image i in button)
+			{
+                invisible = i.color;
+                invisible.a = 255;
+                i.color = invisible;
+            }
+            invisible = point.color;
+            invisible.a = 255;
+            point.color = invisible;
+            demo.alpha = 0;
+        }
+	}
+	private void Awake()
+	{
+        demo.text = play;
+        status = false;
+        demonstration(status);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        point.rectTransform.anchoredPosition = new Vector2(-130, 90);
+        order = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!status)
+        {
+			
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (order > 0)
+                {
+                    point.rectTransform.anchoredPosition += vec;
+                    order--;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (order < 2)
+                {
+                    point.rectTransform.anchoredPosition -= vec;
+                    order++;
+                }
+            }
+        }
+        if (status) 
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+		    {
+                status = false;
+                demonstration(status);
+            }
+        }
+        if(Input.GetKeyUp(KeyCode.Return))
+        {
+			if (order <= 2)
+			{
+                Debug.Log(order);
+                //Destroy(btn);
+				switch (order)
+				{
+                    case 0:
+                        SceneManager.LoadScene("Select Scene");
+                        break;
+                    case 1:
+                        status = true;
+                        demonstration(status);
+                        break;
+                    case 2:
+                        Application.Quit();
+                        break;
+                    default:
+                        break;
+				}
+			}
+		}
+    }
+    
+}
